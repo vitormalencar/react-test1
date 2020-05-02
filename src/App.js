26cd32535b15f9c5e6fce59dcbe2ded8f483b432
vitormalencar <vitor.maia.alencar@gmail.com>
2020-05-02 15:11:17 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import faker from "faker";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await api.get("repositories");
+      setRepositories(data);
+    }
+    fetchData();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    let techs = faker.random.words().split(" ");
+    const { data } = await api.post("repositories", {
+      title: faker.internet.userName(),
+      url: faker.internet.url(),
+      techs: techs,
+    });
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const repoList = repositories.filter((repo) => repo.id !== id);
+
+    setRepositories(repoList);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            <h1>{repo.title}</h1>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
